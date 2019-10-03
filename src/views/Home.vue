45320726987bd412a24fe254ca3f78b0956b6a3b
dani <dani.zaghian@gmail.com>
2019-10-03 09:39:42 -0700

    adds axios request for all recipes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
+
+
     <h1>{{ message }}</h1>
+
+    <div v-for="recipe in recipes">
+      <h2>Title: {{ recipe.title }}</h2>
+      <p>Ingredients: {{ recipe.ingredients }}</p>
+      <p>Directions: {{ recipe.directions }}</p>
+    </div>
+
+
+
   </div>
 </template>
 
@@ -8,13 +19,21 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      recipes: []
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("/api/recipes").then(response => {
+      this.recipes = response.data;
+      console.log(this.recipes);
+    });
+  },
   methods: {}
 };
 </script>
